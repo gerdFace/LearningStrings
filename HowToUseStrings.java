e78b64cb6a033aefa8fd96b655d8663f71083bb8
Steven Gerdes <stevenagerdes@gmail.com>
2016-11-01 14:18:21 -0500

    New Coding Bat Problems

--- HowToUseStrings.java
+++ HowToUseStrings.java
@@ -1,4 +1,3 @@
-
 public class HowToUseStrings {
     public HowToUseStrings() {
     }
@@ -33,10 +32,51 @@ public class HowToUseStrings {
         return noBs;
     }
 
+/*CodingBat problem --must return first and second character if they
+* are 'o' or 'z', respectively.*/
+    public String startOz(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0)== 'o') {
+            result = result + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)== 'z') {
+            result = result + str.charAt(1);
+        }
+        return result;
+        }
+
     public String delDel(String str) {
-        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
-            return str.substring(0, 1) + str.substring(4);
+            if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+                return str.substring(0, 1) + str.substring(4);
+            }
+            return str;
+    }
+
+//return String word in the middle of String out
+    public String makeOutWord(String out, String word) {
+        String firstPart = out.substring(0, 2);
+        String lastPart = out.substring(2);
+        return firstPart.concat(word).concat(lastPart);
+    }
+//return 3 copies of last two letters of str
+    public String extraEnd(String str) {
+        String newStr = str.substring(Math.max(str.length() - 2, 0));
+        return newStr.concat(newStr).concat(newStr);
+}
+/*return first two chars of a string --if less than 2 chars long return
+    original string*/
+    public String firstTwo(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
         }
         return str;
+    }
+
+/*given a string of even length return the first half*/
+    public String firstHalf(String str) {
+        int firstPart = str.length() / 2;
+        return str.substring(0, firstPart);
     }
 }
